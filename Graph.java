44a84d163714f96ad387535c8d5dd2c9747cf6d9
OLI <111159933+fausergit@users.noreply.github.com>
2022-09-03 01:06:40 +0000

    Aula 02/09

--- Graph.java
+++ Graph.java
@@ -1,15 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class Graph {
 
-  private int countNodes;// vertice
-  private int countEdges;// aresta
+  private int countNodes;
+  private int countEdges;
   private int[][] adjMatrix;
 
   public Graph(int numNodes) {
     this.countNodes = numNodes;
     this.countEdges = 0;
     this.adjMatrix = new int[numNodes][numNodes];
+  }
+
+  public Graph(String fileName) throws IOException {
+    File file = new File(fileName);
+    FileReader reader = new FileReader(file);
+    BufferedReader bufferedReader = new BufferedReader(reader);
+
+    // Read header
+    String[] line = bufferedReader.readLine().split(" ");
+    this.countNodes = (Integer.parseInt(line[0]));
+    int fileLines = (Integer.parseInt(line[1]));
+    // Create and fill adjMatrix with read edges
+    this.adjMatrix = new int[this.countNodes][this.countNodes];
+    for (int i = 0; i < fileLines; ++i) {
+      String[] edgeInfo = bufferedReader.readLine().split(" ");
+      int source = Integer.parseInt(edgeInfo[0]);
+      int sink = Integer.parseInt(edgeInfo[1]);
+      int weight = Integer.parseInt(edgeInfo[2]);
+      addEdge(source, sink, weight);
+    }
+    bufferedReader.close();
+    reader.close();
   }
 
   public void addEdge(int source, int sink, int weight) {
@@ -21,6 +47,18 @@ public class Graph {
     }
     this.countEdges++;
     this.adjMatrix[source][sink] = weight;
+  }
+
+  public void addEdgeUnoriented(int source, int sink, int weight) {
+    if (source < 0 || source > this.adjMatrix.length - 1 ||
+        sink < 0 || sink > this.adjMatrix.length - 1 ||
+        weight <= 0) {
+      System.err.printf("Invalid edge: %d %d %d\n", source, sink, weight);
+      return;
+    }
+    this.countEdges += 2;
+    this.adjMatrix[source][sink] = weight;
+    this.adjMatrix[sink][source] = weight;
   }
 
   public int degree(int node) {
@@ -46,17 +84,6 @@ public class Graph {
     return highest;
   }
 
-  public String toString() {
-    String str = "";
-    for (int i = 0; i < this.adjMatrix.length; ++i) {
-      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
-        str += this.adjMatrix[i][j] + "\t";
-      }
-      str += "\n";
-    }
-    return str;
-  }
-
   public int lowestDegree() {
     int lowest = this.countNodes;
     for (int i = 0; i < this.adjMatrix.length; ++i) {
@@ -80,38 +107,180 @@ public class Graph {
   }
 
   public float density() {
-    float densidade = Math.abs(this.countEdges) / (Math.abs(this.countNodes)) * (Math.abs(this.countNodes) - 1);
-    return densidade;
+    return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
   }
 
-  public void addEdgeUnoriented(int source, int sink, int weight) {
-    if (source < 0 || source > this.adjMatrix.length - 1 ||
-        sink < 0 || sink > this.adjMatrix.length - 1 ||
-        weight <= 0) {
-      System.err.printf("Invalid edge: %d %d %d\n", source, sink, weight);
-      return;
+  public boolean oriented() {
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+        if (this.adjMatrix[i][j] != this.adjMatrix[j][i])
+          return true;
+      }
     }
-    this.countEdges += 2;
-    this.adjMatrix[source][sink] = weight;
-    this.adjMatrix[sink][source] = weight;
+    return false;
   }
 
-  public void buscaEmLargura(int ursprung)
-  {
-    int desc [ ] = new int [ this.countNodes ];
-      //criar um array vazio
-      ArrayList<Integer> q =new ArrayList<Integer>();
-       ArrayList<Integer> r =new ArrayList<Integer>();
-      
-      q.add(ursprung);  //origem
-      r.add(ursprung) //pesquisa
-      
-      while (!q.isEmpty() ) {
-      int v = q.remove(0);
-
-      for (int v = 0; v < this.adjMatrix[u].length++) {
-          if( this.adjMatrix[u][y] !=0);
+  public ArrayList<Integer> bfs(int s) { // busca em largura
+    // initialization
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> Q = new ArrayList<>();
+    Q.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+    // main loop
+    while (Q.size() > 0) {
+      int u = Q.remove(0);
+      for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+        if (this.adjMatrix[u][v] != 0) { // v é adjacente a u
+          if (desc[v] == 0) {
+            Q.add(v);
+            R.add(v);
+            desc[v] = 1;
+          }
+        }
       }
-      }
+    }
+    return R;
   }
+
+  public int notDescAdj(int u, int[] desc) {
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0 && desc[v] == 0)
+        return v;
+    }
+    return -1;
+  }
+
+  public ArrayList<Integer> dfs(int s) { // busca em profundidade
+    // initialization
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> S = new ArrayList<>();
+    S.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+    // main loop
+    while (S.size() > 0) {
+      int u = S.get(S.size() - 1);
+      int v = notDescAdj(u, desc);
+      if (v != -1) {
+        S.add(v);
+        R.add(v);
+        desc[v] = 1;
+      } else {
+        S.remove(S.size() - 1);
+      }
+    }
+    return R;
+  }
+
+  public boolean connected() {
+    return this.bfs(0).size() == this.countNodes;
+  }
+
+  public int verificaAdjacente(int u, int[] desc) {
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0) {
+        if (desc[v] == 0) {
+          return v;
+        }
+      }
+    }
+    return -1;
+  }
+
+  public boolean hasCycle(int origem) {
+    List<Integer> q = new ArrayList<Integer>();
+    List<Integer> r = new ArrayList<Integer>();
+    int desc[] = new int[this.countNodes];
+    q.add(origem);
+    r.add(origem);
+    desc[origem] = 1;
+
+    while (!q.isEmpty()) {
+      int u = q.remove(0);
+      int v = verificaAdjacente(u, desc);
+      if (v != -1) {
+        q.add(v);
+        r.add(v);
+        desc[v] = 1;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public ArrayList<Integer> dfsRec(int s) {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> R = new ArrayList<>();
+    dfsRecAux(s, desc, R);
+    return R;
+  }
+
+  public void dfsRecAux(int u, int[] desc, ArrayList<Integer> R) {
+    desc[u] = 1;
+    R.add(u);
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+        dfsRecAux(v, desc, R);
+      }
+    }
+  }
+
+  // ordenação topológica só se aplica a DAE
+  public ArrayList<Integer> topSort() {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> R = new ArrayList<>();
+    for (int v = 0; v < this.adjMatrix.length; ++v) {
+      if (desc[v] == 0)
+        topSortAux(v, desc, R);
+    }
+    return R;
+  }
+
+  public void topSortAux(int u, int[] desc, ArrayList<Integer> R) {
+    desc[u] = 1;
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0 && desc[v] == 0)
+        topSortAux(v, desc, R);
+    }
+    R.add(0, u);
+  }
+
+  public int[] connectedComp() {
+    int[] desc = new int[this.countNodes];
+    int comp = 0;
+    for (int v = 0; v < this.adjMatrix.length; ++v) {
+      if (desc[v] == 0) {
+        comp++;
+        connectedCompAux(v, desc, comp);
+      }
+    }
+    return desc;
+  }
+
+  public void connectedCompAux(int u, int[] desc, int comp) {
+    desc[u] = comp;
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0) {
+        if (desc[v] == 0) {
+          connectedCompAux(v, desc, comp);
+        }
+      }
+    }
+  }
+
+  public String toString() {
+    String str = "";
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+        str += this.adjMatrix[i][j] + "\t";
+      }
+      str += "\n";
+    }
+    return str;
+  }
+
 }
--- Main.java
+++ Main.java
@@ -1,21 +1,45 @@
 class Main {
   public static void main(String[] args) {
-    // System.out.println("Hello world!");
     Graph g1 = new Graph(4);
-    g1.addEdge(0, 1, 3);
-    g1.addEdge(1, 0, 3);
-    g1.addEdge(0, 3, 4);
-    g1.addEdge(3, 0, 4);
-    g1.addEdge(0, 4, 1); // Warning
-    System.out.println(g1);
-    g1.degree(0); // 2
-    g1.degree(1); // 1
-    g1.degree(2); // 0
-    g1.degree(3); // 1
 
-    System.out.println(g1.highestDegree()); // 2
-    System.out.println(g1.lowestDegree()); // 0
-    System.out.println(g1.complement());
-    System.out.println(g1.densidade());
+    g1.addEdgeUnoriented(6, 3, 1);
+    g1.addEdgeUnoriented(6, 7, 1);
+    g1.addEdgeUnoriented(6, 1, 1);
+    g1.addEdgeUnoriented(3, 4, 1);
+    g1.addEdgeUnoriented(7, 0, 1);
+    g1.addEdgeUnoriented(1, 2, 1);
+    g1.addEdgeUnoriented(0, 5, 1);
+    System.out.println(g1.dfs(2));
+    System.out.println();
+    Graph g2 = new Graph(6);
+    g2.addEdgeUnoriented(0, 1, 1);
+    g2.addEdgeUnoriented(1, 4, 1);
+    g2.addEdgeUnoriented(4, 0, 1);
+    g2.addEdgeUnoriented(3, 5, 1);
+
+    int[] comp = g2.connectedComp();
+
+    for (int i = 0; i < comp.length; ++i)
+
+    {
+      System.out.print(comp[i]);
+
+    }
+
+    System.out.print("\n");
+    System.out.println(g2.hasCycle(3));
+    // g1.addEdge(0, 1, 1);
+    // g1.addEdge(1, 0, 1);
+    // g1.addEdge(0, 3, 1);
+    // g1.addEdge(3, 0, 1);
+    // System.out.println(g1);
+    // g1.degree(0); // 2
+    // g1.degree(1); // 1
+    // g1.degree(2); // 0
+    // g1.degree(3); // 1
+    // System.out.println(g1.highestDegree()); // 2
+    // System.out.println(g1.lowestDegree()); // 0
+    // System.out.println(g1.complement());
+    // System.out.println(g1.oriented());
   }
 }
